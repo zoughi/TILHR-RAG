a09725e595fce09a4fed46c62a889419b1a914b4
mahtabfard <aghdamifard.mahtab@gmail.com>
2025-09-29 12:36:59 +0330

    configuration for preprocessing

---
+++ config.py
@@ -0,0 +1,6 @@
+# --------- تنظیمات مسیرها ---------
+RAW_MATERIALS_DIR = "outputs/Raw_materials"         # پوشه اصلی مواد خام
+MD_OUTPUT_DIR = f"{RAW_MATERIALS_DIR}/md"           # پوشه خروجی فایل‌های Markdown (.md)
+TXT_OUTPUT_DIR = f"{RAW_MATERIALS_DIR}/txt"         # پوشه خروجی فایل‌های متنی (.txt)
+UPLOAD_DIR = "uploads"                              # پوشه فایل‌های آپلود شده توسط کاربر
+LOG_FILE = "processed_files.txt"                    # فایل لاگ برای ثبت فایل‌های پردازش شده
